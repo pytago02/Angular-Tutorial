82f7737ba87c6140ab9a6a82710afc8b574e9356
Trung Khanh <pytago02@gmail.com>
2024-12-10 11:22:42 +0700

    NgIF

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  imports : [NgIf],
 })
 export class HomeComponent {
-  public name = "Nguyen Van B"
+  public name = "Nguyen Van B";
+  public age = 90;
 }
+
