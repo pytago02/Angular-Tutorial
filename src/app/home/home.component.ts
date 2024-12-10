d14cace711a5fb012f5480d0f7ab331719d9e2fd
Trung Khanh <pytago02@gmail.com>
2024-12-10 14:47:20 +0700

    [(ngModel)]

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports : [NgIf],
+  imports: [NgIf, FormsModule],
 })
 export class HomeComponent {
-  public name = "Nguyen Van B";
+  public name = 'Nguyen Van B';
   public age = 90;
-}
 
+  public yourName = 'Nguyen Van A'; 
+
+
+  public resetname() {
+    this.yourName = ''; 
+  }
+}
